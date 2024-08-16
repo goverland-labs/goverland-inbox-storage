6b0d66bc3326e2e29fdf0dd1de4411c8a2ed7f7d
Dmitry <dmitry@goverland.xyz>
2024-08-16 13:07:03 +0300

    Implement AI summary endpoint

--- internal/config/ai.go
+++ internal/config/ai.go
@@ -1,6 +1,6 @@
 package config
 
 type AI struct {
-	MonthlyRateLimit  int64  `env:"MONTHLY_RATE_LIMIT" envDefault:"10"`
-	ExternalClientKey string `env:"EXTERNAL_CLIENT_KEY" require:"true"`
+	MonthlyRateLimit  int64  `env:"AI_MONTHLY_RATE_LIMIT" envDefault:"10"`
+	ExternalClientKey string `env:"AI_EXTERNAL_CLIENT_KEY" require:"true"`
 }
